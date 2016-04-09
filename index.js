f4efa87533d4f4abaf2e7e2d673c479766ae62d0
Jonathan <me@errorspersecond.io>
2016-04-09 16:41:49 -0400

    turn makeRequest into a promise

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 "use strict";
 var config = require("./config.js");
 var request = require("request");
+var endpoints = require("./endpoints.js");
+
+var zones = [];
 
 let BASE_URL = "https://api.cloudflare.com/client/v4/";
 let options = {
@@ -11,7 +14,29 @@ let options = {
 	}
 };
 
-function makeRequest(url, cb) {
+function makeRequest(url,  method) {
 	options.url = BASE_URL + url;
-	request.get(options, cb);
+	options.method = method;
+
+	return new Promise( (resolve, reject) => {
+		request(options, (err, res, body) => {
+			let response = JSON.parse(body);
+			if (!response.success) {
+				reject(response.errors);
+			}
+			resolve(response.result);
+			});
+	});
 }
+
+function getZones() {
+	makeRequest(endpoints.zones,  'GET').then((result) => {
+		result.forEach(val) {
+			zones.push(id);
+		}
+	})
+	.catch( (err) => {
+		console.log(err);;
+	})
+}
+
