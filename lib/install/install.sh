3fb963869ff83bc6bbea0c214a51c3777ce04ee9
Jonathan <me@errorspersecond.io>
2016-04-19 18:39:28 -0400

    add npm path lookup for service

--- lib/install/install.sh
+++ lib/install/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo cp ./lib/install/pi-online.service /etc/systemd/system/pi-online.service
+sudo cp ./lib/install/pi-online.service /etc/systemd/system/pi-online.service &&
+sudo  systemctl enable pi-online.service &&
+sudo systemctl start pi-online.service &
